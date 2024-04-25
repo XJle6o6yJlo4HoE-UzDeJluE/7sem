8833e1f23784fecfa92b3229259a0374ceb1d28b
3JlOu_XJlE6yWEK <165206563+XJle6o6yJlo4HoE-UzDeJluE@users.noreply.github.com>
2024-04-25 11:29:44 +0300

    Add files via upload

--- 7-1-measure.py
+++ 7-1-measure.py
@@ -4,8 +4,9 @@ import time
 
 leds = [2, 3, 4, 17, 27, 22, 10, 9]
 dac  = [8, 11, 7, 1, 0, 5, 12, 6]
-troyka = 17
+troyka = 13
 comp = 14
+
 bits = len(dac)
 levels = 2 ** bits
 
@@ -18,24 +19,15 @@ def dec2bin(val):
     return [int(i) for i in bin(val)[2:].zfill(8)]
 
 def adc():
-    # level = 0
-    # for i in range(bits -1, -1, -1):
-    #     level += 2**i
-    #     GPIO.output(dac, dec2bin(level))
-    #     time.sleep(0.01)
-    #     comp_val = GPIO.input(comp)
-    #     if (comp_val == 0):
-    #         level -= 2**i
-    #     return level
-    i = 255
-    val = 0
-    while i > 1:
-        GPIO.output(dac, dec2bin(val + i)[0:8])
+    level = 0
+    for i in range(bits - 1, -1, -1):
+        level += 2**i
+        GPIO.output(dac, dec2bin(level))
         time.sleep(0.01)
-        if GPIO.input(comp) == GPIO.LOW:
-            val += i
-        i //= 2
-    return val
+        comp_val  = GPIO.input(comp)
+        if (comp_val == 1):
+            level -= 2**i
+    return level
 
 def bin_num_leds(val):
     aboba = dec2bin(val)
@@ -46,26 +38,53 @@ data = []
 data_times = []
 
 try:
-    GPIO.output(troyka, 0)
-    time1 = time.time()
+    # GPIO.output(troyka, 1)
+    # time1 = time.time()
+    # val = 0
+    # while(val < 200):
+    #     val = adc()
+    #     bin_num_leds(val)
+    #     data.append(val)
+    #     data_times.append(time.time() - time1)
+    #     print("volt - {:3}".format(val / levels * 3.3))
+    # GPIO.output(troyka, 0)
+    # while (val > 2):
+    #     val = adc()
+    #     bin_num_leds(val)
+    #     data.append(val)
+    #     data_times.append(time.time() - time1)
+    # time2 = time.time()
+    # with open("./settings.txt", "w") as f:
+    #     f.write(str((time2 - time1) / len(data)))
+    #     f.write("\n")
+    #     f.write(str(3.3 / 256))
+    start_time = time.time()
     val = 0
-    while(val < 50):
-        val = adc()
-        bin_num_leds(val)
-        data.append(val)
-        data_times.append(time.time() - time1)
-        print("volt - {:3}".format(val / levels * 3.3))
+
     GPIO.output(troyka, 1)
-    while (val > 20):
+    while(val < 75):
         val = adc()
+        print(val, "volts - {:3}".format(val / levels * 3.3))
         bin_num_leds(val)
         data.append(val)
-        data_times.append(time.time() - time1)
-    time2 = time.time()
-    with open("./settings.txt", "w") as f:
-        f.write(str((time2 - time1) / len(data)))
-        f.write("\n")
-        f.write(str(3.3 / 256))
+        data_times.append(time.time() - start_time)
+
+    GPIO.output(troyka, 0)
+    while(val > 50 and val < 76):
+        val = adc()
+        print(val, "**volts - {:3}".format(val/levels * 3.3))
+        bin_num_leds(val)
+        data.append(val)
+        data_times.append(time.time() - start_time)
+
+    end_time = time.time()
+
+    with open("./settings.txt", "w") as file:
+        file.write(str((end_time - start_time) / len(data)))
+        file.write(("\n"))
+        file.write(str(3.3 / 256))
+
+    print(end_time - start_time, " time\n", len(data) / (end_time - start_time), "\n", 3.3 / 256)
 
 finally:
     GPIO.output(dac, GPIO.LOW)
