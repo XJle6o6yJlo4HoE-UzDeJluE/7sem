82babb1554dcb58fc2cd86f6a3b437668dba194d
3JlOu_XJlE6yWEK <165206563+XJle6o6yJlo4HoE-UzDeJluE@users.noreply.github.com>
2024-04-25 10:57:40 +0300

    Add files via upload

---
+++ 7-1-measure.py
@@ -0,0 +1,82 @@
+import RPi.GPIO as GPIO
+import matplotlib.pyplot as plt
+import time
+
+leds = [2, 3, 4, 17, 27, 22, 10, 9]
+dac  = [8, 11, 7, 1, 0, 5, 12, 6]
+troyka = 17
+comp = 14
+bits = len(dac)
+levels = 2 ** bits
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(comp, GPIO.IN)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.LOW)
+
+def dec2bin(val):
+    return [int(i) for i in bin(val)[2:].zfill(8)]
+
+def adc():
+    # level = 0
+    # for i in range(bits -1, -1, -1):
+    #     level += 2**i
+    #     GPIO.output(dac, dec2bin(level))
+    #     time.sleep(0.01)
+    #     comp_val = GPIO.input(comp)
+    #     if (comp_val == 0):
+    #         level -= 2**i
+    #     return level
+    i = 255
+    val = 0
+    while i > 1:
+        GPIO.output(dac, dec2bin(val + i)[0:8])
+        time.sleep(0.01)
+        if GPIO.input(comp) == GPIO.LOW:
+            val += i
+        i //= 2
+    return val
+
+def bin_num_leds(val):
+    aboba = dec2bin(val)
+    GPIO.output(dac, aboba)
+    return aboba
+
+data = []
+data_times = []
+
+try:
+    GPIO.output(troyka, 0)
+    time1 = time.time()
+    val = 0
+    while(val < 50):
+        val = adc()
+        bin_num_leds(val)
+        data.append(val)
+        data_times.append(time.time() - time1)
+        print("volt - {:3}".format(val / levels * 3.3))
+    GPIO.output(troyka, 1)
+    while (val > 20):
+        val = adc()
+        bin_num_leds(val)
+        data.append(val)
+        data_times.append(time.time() - time1)
+    time2 = time.time()
+    with open("./settings.txt", "w") as f:
+        f.write(str((time2 - time1) / len(data)))
+        f.write("\n")
+        f.write(str(3.3 / 256))
+
+finally:
+    GPIO.output(dac, GPIO.LOW)
+    GPIO.output(troyka, GPIO.LOW)
+    GPIO.cleanup()
+
+data_str = [str(i) for i in data]
+data_times_str = [str(i) for i in data_times]
+
+with open("./data.txt", "w") as f:
+    f.write("\n".join(data_str))
+
+plt.plot(data_times, data)
+plt.show()
